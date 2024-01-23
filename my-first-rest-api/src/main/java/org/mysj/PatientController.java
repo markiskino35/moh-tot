f5cedb6c45b42f6b75c4803f52586f6b7874fcc4
Admin <markiskino35@gmail.com>
2024-01-23 15:53:25 +0800

    first commit

---
+++ my-first-rest-api/src/main/java/org/mysj/PatientController.java
@@ -0,0 +1,22 @@
+package org.mysj;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mysj.dto.Patient;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PatientController {
+
+    private final Map<Integer, Patient> map = new HashMap<>();
+
+    @PostMapping("/patient")
+    void createPatient(@RequestBody Patient patient){
+        map.put(patient.getPatientNo(), patient);
+    }
+
+    @GetMapping("/patient/{id}")
+    Patient getPatient(@PathVariable("id") Integer patientId){
+        return map.get(patientId);
+    }
+}
